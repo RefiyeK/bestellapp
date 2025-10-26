943a2f4eeb64da954fe6b6c361aa403970407955
RefiyeK <205158404+RefiyeK@users.noreply.github.com>
2025-10-26 21:52:03 +0100

    Add restaurants and dish data structure

---
+++ js/data.js
@@ -0,0 +1,66 @@
+const restaurant = {
+    name : "Restaurant ismi",
+    logo : "logo.png",
+    rating : 4.7,
+    deliveryCost : 3.50
+};
+
+
+const dishes = [
+    {
+        "name": "Pizza Margherita",
+        "category": "Pizza",
+        "price": 7.50,
+        "description": "Die Margherita ist eine neapolitanische Variante."
+    },
+    {
+        "name": "Vier-Käse-Pizza",
+        "category": "Pizza",
+        "price": 9.50,
+        "description": "Pizza Quattro Formaggi: vier verschiedene Käsesorten. "
+    },
+    {
+        "name": "Pizza Salami",
+        "category": "Pizza",
+        "price": 9.50,
+        "description": "Pizza Quattro Formaggi: vier verschiedene Käsesorten. "
+    },
+    {
+        "name": "Gemischter Salat",
+        "category": "Salat",
+        "price": 7.50,
+        "description": "Alla Salate werden mit grünem Salat, frischen Tomaten, Gurken und einem Dressing Ihrer Wahl zubereitet."
+    },
+    {
+        "name": "Capricciosa Salat",
+        "category": "Salat",
+        "price": 9.50,
+        "description": "Mit mais, paprika und Zwiebeln "
+    },
+    {
+        "name": "Tonno Salat",
+        "category": "Salat",
+        "price": 9.50,
+        "description": "Mit Käse, Putenschinken, Thunfisch, Mais, ei und Zwiebeln."
+    },
+    {
+        "name": "Aglio e Olio",
+        "category": "Pasta",
+        "price": 10.50,
+        "description": "Mit Knoblauch, Jalapenos, Olivenöl und Parmesan"
+    },
+    {
+        "name": "Pasta alla Panna",
+        "category": "Pasta",
+        "price": 10.90,
+        "description": "Mit Putenschinken, Champignons und Sahnesauce"
+    },
+    {
+        "name": "Pasta Vegataria",
+        "category": "Pasta",
+        "price": 10.90,
+        "description": "Mit Gemüse und Tomatensauce"
+    }
+];
+
+const basket = [];
