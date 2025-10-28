861caf6784257add195a8e0738ab37ac1f9183b5
RefiyeK <205158404+RefiyeK@users.noreply.github.com>
2025-10-28 10:20:07 +0100

    body (main und aside

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function addDishes(index){
+    console.log("newDishes:", dishes[index]);
+    console.log("sepet şu an:", basket);
+}
+
+function myBasket(){}
+
+function addBasket(){}
+
+function updateBasket(){}
+
